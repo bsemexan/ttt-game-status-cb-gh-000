8bed57da9844c6b1d27bd2b8e543390b9fb848a6
Berry  Semexan <bsemexan@gmail.com>
2018-02-08 02:25:35 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,7 +7,12 @@ end
 IN_COMBINATIONS = [
   [0,1,2], # Top row
   [3,4,5]  # Middle row
-  # ETC, an array for each win combination
+  [6,7,8]
+  [0,3,6]
+  [1,4,7]
+  [2,5,8]
+  [0,4,8]
+  [2,4,6] # ETC, an array for each win combination
 ]
 
 def won?(board)
@@ -24,3 +29,5 @@ end
 
 def winner(board)
 end
+
+
